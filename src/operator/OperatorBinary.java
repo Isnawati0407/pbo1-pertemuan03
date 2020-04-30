40c17e6923c5f348255268f7ad21bad9ddc8b220
isnawati <isnawati222@gmail.com>
2020-04-30 12:57:40 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Mirza Yogy Kurniawan";
+    int jumlahSks = 24;
+    Double indeksPrestasiKumulatif = 3.89;
+    
+}
