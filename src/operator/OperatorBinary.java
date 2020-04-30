01ab083e18ddad9221cde3491799c53f6a4ec25d
isnawati <isnawati222@gmail.com>
2020-04-30 13:01:46 +0800

    Operator Binary Aritmatika

--- src/operator/OperatorBinary.java
+++ src/operator/OperatorBinary.java
@@ -9,5 +9,14 @@ public class OperatorBinary {
     String nama = "Mirza Yogy Kurniawan";
     int jumlahSks = 24;
     Double indeksPrestasiKumulatif = 3.89;
-    
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+
 }
