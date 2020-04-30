ccee922ddda10b4edf65b536307bcb1114a68b34
isnawati <isnawati222@gmail.com>
2020-04-30 13:52:16 +0800

    IfElse 2

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,15 +2,16 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 53000;
-        double uangDiDompet = 25000;
+        double totalBelanja = 25000;
+        double uangDiDompet = 50000;
 
-        if(uangDiDompet<totalBelanja) {
+
+
+        if (uangDiDompet < totalBelanja) {
             System.out.println("Uang kurang, kurangi jajanan anda");
-        }else{
-            System.out.println("uang cukup, selamat menikamti");
-                }
-
+        } else {
+            System.out.println("uang cukup, berelaan lah");
+        }
     }
-
 }
+
