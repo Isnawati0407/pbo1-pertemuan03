9f6e99a64fbea6b3e11acd0a99706d205b764745
isnawati <isnawati222@gmail.com>
2020-04-30 14:07:28 +0800

    percabangan Switch 1

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=3;
+        switch(jalanJalan){
+            case 1:
+                System.out.println("urang lapas");
+                break;
+            case 2:
+                System.out.println("urang acayan");
+                break;
+            case 3:
+                System.out.println("ubat curuna");
+                break;
+            case 4:
+                System.out.println("iwak pakasam");
+                break;
+            default:
+                System.out.println("hantu banyu");
+                break;
+        }
+    }
+}
